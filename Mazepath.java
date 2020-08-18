fcfdbf246a382872546ebc6a77360d4d704e7efc
Prashant2523 <49652839+Prashant2523@users.noreply.github.com>
2020-08-18 16:41:07 +0530

    no of path possible in given codition

--- Mazepath.java
+++ Mazepath.java
@@ -1,22 +1,22 @@
 class Mazepath
 {
-	public static void countpath(int dr, int dc, int sr, int sc,String sp)
+	public static int countpath(int dr, int dc, int sr, int sc,String sp)
 	{
-		if(sr==dr && sc==dc)
-			System.out.println(sp);
+		if(sr==dr && sc==dc) return 1;
+			//System.out.println(sp);
 		if(sr>dr || sc>dc) return ;
-		 countpath(dr,dc,sr,sc+1,"hrm");
+		 int hr=countpath(dr,dc,sr,sc+1,"hrm");
 		//System.out.println(sr+","+sc+"hrm");
-		 countpath(dr,dc,sr+1,sc,"vrm");
-		 countpath(dr,dc,sr+1,sc+1,"Dm");
+		int vr=countpath(dr,dc,sr+1,sc,"vrm");
+		int dg= countpath(dr,dc,sr+1,sc+1,"Dm");
 		//System.out.println(sr+","+sc+"vrmove");
-		//int result= hr+vr;
+		int result= hr+vr+dg;
 		//System.out.println(sr+","+sc+"vrmove");
-		//return result;
+		return result;
 		//int vr= countpath(dr,dc,sr+1,sc);
 
 	}
 	public static void main(String[] args) {
-		Mazepath.countpath(2,2,0,0,"");
+		System.out.print(Mazepath.countpath(2,2,0,0,""));
 	}
 }

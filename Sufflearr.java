dd99a61216e489214eaecae3c075e1e873ac7b03
Prashant2523 <49652839+Prashant2523@users.noreply.github.com>
2020-08-24 09:45:50 +0530

    Leet. 1470. Shuffle the Array

---
+++ Sufflearr.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        
+        int[] shuffled = new int[nums.length];
+        
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            if (i%2 == 0) {
+                shuffled[i] = nums[j];
+            } else { 
+                shuffled[i] = nums[j+n];
+                j++;
+            }
+        }
+        
+        return shuffled;
+        
+    }
+}
